bb9bfe91048d5aa3e1d71e1de42d1167d96a56e6
Leslie Fabishilaki <leslie.fab85@gmail.com>
2022-10-18 13:00:21 +0200

    added UI for attaching images in chat

--- swapshop/src/screens/ChatScreen.js
+++ swapshop/src/screens/ChatScreen.js
@@ -1,7 +1,7 @@
 import { StatusBar } from "expo-status-bar";
 import React, {useState, useEffect, useCallback, useLayoutEffect} from 'react';
 import {View, ScrollView, Text, Button, StyleSheet} from 'react-native';
-import {Bubble, GiftedChat, Send} from 'react-native-gifted-chat';
+import {Bubble, GiftedChat, Send, Actions, ActionsProps} from 'react-native-gifted-chat';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
 import colors from "../../config/colors";
@@ -14,14 +14,85 @@ import { login_user } from "../classes/User_Account";
 // this will make use of React-Native Gifted Chats (a builtin package which handles the components of the chat screen interface)
 const ChatScreen = ({route, navigation}) => {
   const [messages, setMessages] = useState([]);
+  const[image, setImage] = useState(null);
+  const [imageList, setImgaeList] = useState('');
   const item = route.params;
   
   let username_1 = login_user.getUsername();
   let username_2 = item.owner.getUsername()
-
   let db_tmp = username_1 + username_2;
   let chat_database = db_tmp.split("").sort().join("");
 
+  const pickImage = async ({navigate}) => {
+    //the uploaded image.
+    //uploaded image contents
+
+    ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.All,
+        // allowsEditing: true,
+        allowsMultipleSelection: true,
+        aspect: [4, 3],
+        quality: 0.2,
+        base64: true,
+    })
+    .then((result) => {
+        if (!result.selected) {
+            let t = {"cancelled": result.cancelled, "selected" : [result]};
+            result = t;
+        }
+        if (!result.cancelled) {
+        let temp = [];
+        let tempImageList = [];
+        for (let i = 0; i < result.selected.length; i++) {
+            let uri = result.selected[i].uri;
+            // uri = uri.replace('file://', "");
+            tempImageList.push(result.selected[i]);
+            let item = <Image style={{position:'relative', height: 200, margin: 20}} key={i} source={{
+                uri: uri
+            }}/>;
+            temp.push(item);
+        }
+        setImage(temp);
+        setImgaeList(tempImageList);
+    }
+    }) //well get the log if the process in unsuccessful so we know where the error is:)
+};
+
+const takePicture = async () => {
+
+    await Promise.all([
+        Permissions.askAsync(Permissions.CAMERA),
+    ])
+    // the taken image
+    //upload image contents
+    ImagePicker.launchCameraAsync({
+        allowsEditing: true,
+        aspect: [4, 3],
+        quality: 0.2,
+        base64: true,  
+    }).then((result) => {
+        if (!result.selected) {
+            let t = {"cancelled": result.cancelled, "selected" : [result]};
+            result = t;
+        }
+        if (!result.cancelled) {
+        let temp = [];
+        let tempImageList = [];
+        for (let i = 0; i < result.selected.length; i++) {
+            let uri = result.selected[i].uri;
+            // uri = uri.replace('file://', "");
+            tempImageList.push(result.selected[i]);
+            let item = <Image style={{position:'relative', height: 200, margin: 20}} key={i} source={{
+                uri: uri
+            }}/>;
+            temp.push(item);
+        }
+        setImage(temp);
+        setImgaeList(tempImageList);
+    }
+    })
+  }
+  
   useEffect(() => {
 
     // the collection of messages (in firestore database) is ordered in descending order by the time the message has been sent
@@ -92,6 +163,22 @@ const ChatScreen = ({route, navigation}) => {
     );
   };
 
+  const renderActions = (props) => {
+    return (
+      <Actions
+        {...props}
+        options={{
+          /*['Attach Image']: pickImage,*/
+        }}
+        icon={() => (
+          <View>
+            <MaterialCommunityIcons name={'image'} size={28} color={colors.mediumAquaMarine} />
+          </View>
+        )}
+      />
+    )
+  }
+
   // render the chat bubble
   const renderBubble = (props) => {
     return (
@@ -139,6 +226,7 @@ const ChatScreen = ({route, navigation}) => {
       renderBubble={renderBubble}
       alwaysShowSend
       renderSend={renderSend}
+      renderActions={renderActions}
       scrollToBottom
       scrollToBottomComponent={scrollToBottomComponent}
     />
